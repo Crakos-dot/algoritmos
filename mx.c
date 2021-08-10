16e9b447ce793dba00ca334a77e4c278e2e4951c
Crakos-dot <74736373+Crakos-dot@users.noreply.github.com>
2021-08-10 11:58:34 -0300

    Add files via upload

---
+++ mx.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]) {
+  int a=atoi(argv[1]);
+
+  int contador=1;
+while (contador<=a) {
+  printf("hola mundo\n" );
+  a--;
+}
+
+
+
+  return 0;
+}
